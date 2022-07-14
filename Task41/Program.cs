c474a7b0c2f32a329c2a55b9051985549d0c77ae
iryna <rinakinsha@yndex.by>
2022-07-14 20:50:34 +0300

    Solution of task 41

---
+++ Task41/Program.cs
@@ -0,0 +1,44 @@
+﻿int[] ConversionToArray(int size)
+{
+    while (size <= 0)
+    {
+        Console.Write("А нееет, так не пойдет, введите другое число: ");
+        size = Convert.ToInt32(Console.ReadLine());
+    }
+
+    int[] array = new int[size];
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine("Введите число для " + i + "-го элемента массива: ");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
+
+void ShowArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        Console.Write($"{array[i]} ");
+
+    Console.WriteLine();
+}
+
+int PositiveNumbers(int[] array)
+{
+    int numbers = 0;
+    for (int j = 0; j < array.Length; j++)
+        if (array[j] > 0) numbers++;
+
+    return numbers;
+}
+
+Console.Write("Введите размерность массива (определяйтесь с умом, ведь вводить придется ручками): ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int[] newarray = ConversionToArray(number);
+
+Console.Write("Ваш массив: ");
+ShowArray(newarray);
+
+Console.WriteLine("Количество положительных чисел в массиве: " + PositiveNumbers(newarray));
